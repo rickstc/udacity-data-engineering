b3ed2c30bdddb15efc212cdf576faeda3afaed04
Tim Ricks <ricks.tc@gmail.com>
2021-09-25 01:40:27 -0400

    Some tweaks to DB utilities while reviewing exercise 1.

--- 02-data-modeling/src/lesson1/utils.py
+++ 02-data-modeling/src/lesson1/utils.py
@@ -2,20 +2,35 @@ import psycopg2
 
 
 class DBUtils:
+    """
+    Utilities to 'shortcut' some operations that the provided online development environment would already provide
+    """
     PG_USER = "someuser"
     PG_PASS = "somepass"
-    PG_DB = "test"
+    PG_DB = "tmpdb"
     PG_HOST = "127.0.0.1"
     PG_PORT = "9000"
 
     @classmethod
-    def reset_database(self):
+    def reset_databases(self):
         try:
             conn = self.connect('postgres')
             conn.set_session(autocommit=True)
             cursor = conn.cursor()
-            cursor.execute(f'DROP DATABASE {self.PG_DB}')
+
+            # Delete existing databases
+            DBS_TO_PRESERVE = ('postgres', 'template1', 'template0')
+            cursor.execute('SELECT datname FROM pg_database')
+
+            dbs = cursor.fetchall()
+            for db in dbs:
+                db_name = db[0]
+                if db_name not in DBS_TO_PRESERVE:
+                    cursor.execute(f"DROP DATABASE {db_name}")
+
+            # Create New Database
             cursor.execute(f'CREATE DATABASE {self.PG_DB}')
+
         except Exception as e:
             print(e)
 
