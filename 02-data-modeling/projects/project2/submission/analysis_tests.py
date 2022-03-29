76e61e7559de86ec546b77b155194becba1188ba
Tim Ricks <ricks.tc@gmail.com>
2022-03-29 07:59:31 -0400

    Finish the queries and tests. Begin refactoring for rubric items.

---
+++ 02-data-modeling/projects/project2/submission/analysis_tests.py
@@ -0,0 +1,102 @@
+"""
+File: analysis_tests.py
+Attribution: Student
+
+Purpose:
+The project only requires the student to demonstrate successful query execution
+using print statements, however the student would prefer to guarantee consistent
+execution against the data set using test cases.
+"""
+
+import unittest
+from song_analysis import UdacityUtils, query_one, query_two, query_three, create_keyspace, execute_query
+
+
+class SongAnalysisProjectTests(unittest.TestCase):
+    def setUp(self):
+        """ Runs at the beginning of each test """
+        self.cluster, self.session = UdacityUtils.open_db_connection()
+        self.keyspace = create_keyspace(self.session)
+        self.session.set_keyspace(self.keyspace)
+
+    def tearDown(self):
+        """ Runs after every test """
+        UdacityUtils.close_db_connection(self.cluster, self.session)
+
+    def test_query_one(self):
+        """
+        Expected Results:
+        Row(artist_name='Faithless', song_name='Music Matters (Mark Knight Dub)', length=495.30731201171875)
+        """
+        rows = query_one(self.session)
+        num_results = 0
+        for row in rows:
+            num_results += 1
+            self.assertEqual(row.artist_name, 'Faithless')
+            self.assertEqual(row.song_name,
+                             'Music Matters (Mark Knight Dub)')
+            self.assertEqual(row.length, 495.30731201171875)
+        self.assertEqual(num_results, 1)
+
+    def test_query_two(self):
+        """
+        Expected Results:
+        Row(artist_name='Down To The Bone', song_name="Keep On Keepin' On", first_name='Sylvie', last_name='Cruz')
+        Row(artist_name='Three Drives', song_name='Greece 2000', first_name='Sylvie', last_name='Cruz')
+        Row(artist_name='Sebastien Tellier', song_name='Kilometer', first_name='Sylvie', last_name='Cruz')
+        Row(artist_name='Lonnie Gordon', song_name='Catch You Baby (Steve Pitron & Max Sanna Radio Edit)', first_name='Sylvie', last_name='Cruz')
+        """
+        results = [{
+            "artist_name": "Down To The Bone",
+            "song_name": "Keep On Keepin' On",
+        }, {
+            "artist_name": "Three Drives",
+            "song_name": "Greece 2000",
+        }, {
+            "artist_name": "Sebastien Tellier",
+            "song_name": "Kilometer",
+        }, {
+            "artist_name": "Lonnie Gordon",
+            "song_name": "Catch You Baby (Steve Pitron & Max Sanna Radio Edit)",
+        }]
+        rows = query_two(self.session)
+        num_results = 0
+        for row in rows:
+            self.assertEqual(
+                row.artist_name, results[num_results]['artist_name'])
+            self.assertEqual(row.song_name, results[num_results]['song_name'])
+            self.assertEqual(row.first_name, 'Sylvie')
+            self.assertEqual(row.last_name, 'Cruz')
+            num_results += 1
+        self.assertEqual(num_results, 4)
+
+    def test_query_three(self):
+        """
+        Expected Results:
+        Row(last_name='Johnson', first_name='Sara')
+        Row(last_name='Levine', first_name='Tegan')
+        Row(last_name='Lynch', first_name='Jacqueline')
+        """
+
+        results = [{
+            "last_name": "Johnson",
+            "first_name": "Sara"
+        }, {
+            "last_name": "Levine",
+            "first_name": "Tegan"
+        }, {
+            "last_name": "Lynch",
+            "first_name": "Jacqueline"
+        }]
+        rows = query_three(self.session)
+        num_results = 0
+        for row in rows:
+            self.assertEqual(row.last_name, results[num_results]['last_name'])
+            self.assertEqual(
+                row.first_name, results[num_results]['first_name'])
+            num_results += 1
+        self.assertEqual(num_results, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- 02-data-modeling/projects/project2/submission/song_analysis.py
+++ 02-data-modeling/projects/project2/submission/song_analysis.py
@@ -9,21 +9,18 @@ intended to be completed by the student to accomplish the fictional organization
 data analysis goals.
 """
 
-# This should make a connection to a Cassandra instance your local machine
-# (127.0.0.1)
-
 from cassandra.cluster import Cluster
 import csv
 import os
 
 
-class UdacityProvided:
+class UdacityUtils:
     """
-    I abstracted all of the Udacity-provided code to this class
+    The student abstracted all of the Udacity-provided code to this class
     """
     @staticmethod
     def read_datafile():
-        """ I converted the CSV reading portion of Udacity's code to a generator """
+        """ The student converted the CSV reading portion of Udacity's code to a generator """
         csv_file = 'event_datafile_new.csv'
 
         if os.path.exists(csv_file) is False:
@@ -52,37 +49,318 @@ class UdacityProvided:
         cluster.shutdown()
 
 
-# Connect to a session in the cluster
-cluster, session = UdacityProvided.open_db_connection()
-
-# TO-DO: Create a Keyspace
-
-
-# TO-DO: Set KEYSPACE to the keyspace specified above
-
-
-# We have provided part of the code to set up the CSV file. Please complete the Apache Cassandra code below#
+"""
+****************************************** Functions ******************************************
+The student elected to abstract some logic into functions to achieve the following benefits:
+- Consolidating reusable logic allows for error handling with fewer try/except blocks, which can
+clutter code
+- Abstracting some code to functions and creating a main method should make the code more readable
+- Each function could become independently testable
+"""
 
 
-for line in UdacityProvided.read_datafile():
-    print(line)
-# # TO-DO: Assign the INSERT statements into the `query` variable
-#     query = "<ENTER INSERT STATEMENT HERE>"
-#     query = query + "<ASSIGN VALUES HERE>"
-#     # TO-DO: Assign which column element should be assigned for each column in the INSERT statement.
-#     # For e.g., to INSERT artist_name and user first_name, you would change the code below to `line[0], line[1]`
-#     session.execute(query, (line[  # ], line[#]))
+def create_keyspace(session, keyspace_name=None):
+    """
+    Function: create_keyspace
+    Params:
+    - session - A cassandra Cluster's Session object
+    - keyspace_name - A name for the keyspace; if not provided defaults to 'udacity'
 
-# TO-DO: Query 1:  Give me the artist, song title and song's length in the music app history that was heard during \
-# sessionId = 338, and itemInSession = 4
+    Returns: keyspace_name if created successfully, otherwise False
+    """
+    if keyspace_name is None:
+        keyspace_name = "udacity"
+    if keyspace_name.startswith('system'):
+        print("Your chosen keyspace name should not begin with system*")
+        return False
+    try:
+        rep = {'class': 'SimpleStrategy', 'replication_factor': 1}
+        session.execute(
+            f"CREATE KEYSPACE IF NOT EXISTS {keyspace_name} WITH REPLICATION = {rep}")
+        return keyspace_name
+    except Exception as e:
+        print(e)
+        return False
 
-# TO-DO: Query 2: Give me only the following: name of artist, song (sorted by itemInSession) and user (first and last name)\
-# for userid = 10, sessionid = 182
 
-# TO-DO: Query 3: Give me every user name (first and last) in my music app history who listened to the song 'All Hands Against His Own'
+def execute_query(session, query, params=None):
+    """ 
+    Function: execute_query
+    Params:
+    - session - A cassandra Cluster's Session object
+    - query - The query to execute
+    - values (optional) - Any values that need to be passed in to the query
+
+    Returns: The response from session.execute if the query was successful, otherwise None
+
+    Purpose:
+    Wraps session.execute into a try/except block, allowing for the reuse of exception handling
+    """
+    try:
+        if params is not None:
+            return session.execute(query, params)
+        return session.execute(query)
+    except Exception as ex:
+        print(ex)
+        return None
 
 
-# TO-DO: Drop the table before closing out the sessions
+def create_table(session, table_name, primary_key):
+    """
+    Params:
+    - session - The Cassandra Cluster.session object
+    - primary_key - The primary key that will be used with the table
 
-# Close the session and shutdown the cluster
-UdacityProvided.close_db_connection(cluster, session)
+
+    This function consolidates the logic to create a table, as the column names and types
+    do not change for the data; only the primary key changes for each query
+    """
+
+    create_table_query = f"""
+    CREATE TABLE IF NOT EXISTS {table_name} 
+    (
+        session_id int,
+        item_in_session int,
+        user_id int,
+        level text,
+        first_name text,
+        last_name text,
+        gender text,
+        location text,
+        artist_name text,
+        song_name text,
+        length float,
+        PRIMARY KEY {primary_key}
+    )
+    """
+    execute_query(session, create_table_query)
+
+
+def query_one(session):
+    """
+    Params:
+    - session - The Cassandra Cluster.session object
+
+    TO-DO: Query 1:  Give me the artist, song title and song's length in the music app history that was heard during \
+    sessionId = 338, and itemInSession = 4
+
+    The objective of this is to locate the artist, title and length provided a session id and the position in a session
+
+    Therefore, the primary key should contain a partition key of session_id, and a clustering key of item_in_session
+    """
+    # Prepare our primary key and create the table
+    primary_key = "((session_id), item_in_session)"
+    create_table(session, 'session_songs', primary_key)
+
+    # Populate the table
+    for line in UdacityUtils.read_datafile():
+        query = """
+        INSERT INTO session_songs (
+            session_id,
+            item_in_session,
+            user_id,
+            level,
+            first_name,
+            last_name,
+            gender,
+            location,
+            artist_name,
+            song_name,
+            length
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        params = (
+            int(line[8]),
+            int(line[3]),
+            int(line[10]),
+            line[6],
+            line[1],
+            line[4],
+            line[2],
+            line[7],
+            line[0],
+            line[9],
+            float(line[5])
+        )
+        execute_query(session, query, params)
+
+    # Execute the query
+    rows = execute_query(session, """
+        SELECT artist_name, song_name, length
+        FROM session_songs
+        WHERE session_id=%s AND item_in_session=%s
+    """, (338, 4))
+
+    # Drop table to prepare for next query
+    execute_query(session, "DROP TABLE IF EXISTS session_songs")
+    return rows
+
+
+def query_two(session):
+    """
+    Params:
+    - session - The Cassandra Cluster.session object
+
+    TO-DO: Query 2: Give me only the following: name of artist, song (sorted by itemInSession) and user (first and last name)\
+    for userid = 10, sessionid = 182
+
+    The objective of this is to locate the artist, song, and the user's first and last name given a user id and session id
+    These results are to be sorted by the item_in_session
+
+    Therefore, the primary key should be defined as a composite key with a partion key of session_id and clustering keys of
+    user_id and item_in_session
+
+    """
+    primary_key = "((session_id), user_id, item_in_session)"
+    create_table(session, 'user_sessions', primary_key)
+
+    # Populate the table
+    for line in UdacityUtils.read_datafile():
+        query = """
+        INSERT INTO user_sessions (
+            session_id,
+            item_in_session,
+            user_id,
+            level,
+            first_name,
+            last_name,
+            gender,
+            location,
+            artist_name,
+            song_name,
+            length
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        params = (
+            int(line[8]),
+            int(line[3]),
+            int(line[10]),
+            line[6],
+            line[1],
+            line[4],
+            line[2],
+            line[7],
+            line[0],
+            line[9],
+            float(line[5])
+        )
+        execute_query(session, query, params)
+
+    # Execute the query
+    rows = execute_query(session, """
+        SELECT artist_name, song_name, first_name, last_name
+        FROM user_sessions
+        WHERE session_id=%s AND user_id=%s
+        ORDER BY item_in_session
+    """, (182, 10))
+
+    # Drop table to prepare for next query
+    execute_query(session, "DROP TABLE IF EXISTS user_sessions")
+    return rows
+
+
+def query_three(session):
+    """
+    Params:
+    - session - The Cassandra Cluster.session object
+
+    # TO-DO: Query 3: Give me every user name (first and last) in my music app history who listened to the song 'All Hands Against His Own'
+
+    The objective of this is to locate the first and last name of users who listened a given song
+
+    Therefore, the primary key should be a composite containing a partiion key of song_name, with clustering columns
+    of first_name and last_name
+
+    While the problem statement did not dictate any ordering, the student chose to order results by last name
+    """
+    primary_key = "((song_name), last_name, first_name)"
+    create_table(session, 'song_plays', primary_key)
+
+    # Populate the table
+    for line in UdacityUtils.read_datafile():
+        query = """
+        INSERT INTO song_plays (
+            session_id,
+            item_in_session,
+            user_id,
+            level,
+            first_name,
+            last_name,
+            gender,
+            location,
+            artist_name,
+            song_name,
+            length
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+        params = (
+            int(line[8]),
+            int(line[3]),
+            int(line[10]),
+            line[6],
+            line[1],
+            line[4],
+            line[2],
+            line[7],
+            line[0],
+            line[9],
+            float(line[5])
+        )
+        execute_query(session, query, params)
+
+    # Execute the query
+    rows = execute_query(session, """
+        SELECT last_name, first_name
+        FROM song_plays
+        WHERE song_name=%s
+        ORDER BY last_name
+    """, ("All Hands Against His Own", ))
+
+    # Drop table to prepare for next query
+    execute_query(session, "DROP TABLE IF EXISTS song_plays")
+    return rows
+
+
+def init():
+    """
+    The execution of the code required was abstracted into it's own function
+    so that it could be called when the file is run directly from the terminal
+    """
+    # Connect to a session in the cluster
+    cluster, session = UdacityUtils.open_db_connection()
+
+    # TO-DO: Create a Keyspace
+    keyspace = create_keyspace(session)
+
+    # TO-DO: Set KEYSPACE to the keyspace specified above
+    session.set_keyspace(keyspace)
+
+    """
+    At this point, the project requires the student to perform the following tasks:
+    1. Create a table that will be used to store the data from the CSV file
+    2. Read the data in from the CSV file and populate the table
+    3. Write a query that will provide a certain piece of information
+    4. Drop the table after the data has been retrieved
+
+    The student believes that each query might be better abstracted into their its' own
+    function for ease of testing and to improve readability
+    """
+
+    rows = query_one(session)
+    for row in rows:
+        print(row)
+
+    rows = query_two(session)
+    for row in rows:
+        print(row)
+
+    rows = query_three(session)
+    for row in rows:
+        print(row)
+
+    # Close the session and shutdown the cluster
+    UdacityUtils.close_db_connection(cluster, session)
+
+
+if __name__ == '__main__':
+    init()
